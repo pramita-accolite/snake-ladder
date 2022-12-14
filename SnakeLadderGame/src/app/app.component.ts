ef8d76fd344446f0d5be10954f5369cdeef6fffc
pramita-accolite <pramita.bhattacherjee@accolitedigital.com>
2022-12-14 17:12:00 +0530

    moved logic to service

--- SnakeLadderGame/src/app/app.component.ts
+++ SnakeLadderGame/src/app/app.component.ts
@@ -6,6 +6,7 @@ import { PlayersService } from './players.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'SnakeLadderGame';
 
@@ -75,7 +76,7 @@ assignVal2()
       return false;
     }
 
-    
+
 }
 
 
